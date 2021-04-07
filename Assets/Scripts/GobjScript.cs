b5102b1af061f74d6520b468b7fa671db3872bb8
Vlad Mikhalev <vladik.mihalev@yandex.ru>
2021-04-07 14:54:57 +0500

    test

---
+++ Assets/Scripts/GobjScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GobjScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("OK");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
