45242945e9ce78c0d7b078c239cd6c52ff6f2a43
Vurado-t <vladik.mihalev@yandex.ru>
2021-04-12 20:38:13 +0500

    Interface test

---
+++ Assets/Interfaces/IPlayer.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Interfaces
+{
+    public interface IPlayer
+    { 
+        void Move(Vector2 point);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+﻿using Interfaces;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Player : IPlayer
+    {
+        public void Move(Vector2 point)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
