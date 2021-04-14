47e1410076c48ca2761078eb892f0adc02864588
Vurado-t <vladik.mihalev@yandex.ru>
2021-04-14 14:15:30 +0500

    Demo

--- Assets/UnitsScripts/PlayerScript.cs
+++ Assets/UnitsScripts/PlayerScript.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using UnitsClasses;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace UnitsScripts
 {
     public class PlayerScript : UnitScript<Player>
     {
         //TODO: private Book currentBook = new Book(); 
+
+        private Rigidbody2D rigidbody2D;
         
         private void Start()
         {
+            rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
             UnitModel = new Player
             {
                 Health = health,
@@ -19,7 +24,22 @@ namespace UnitsScripts
 
         private void Update()
         {
-            throw new NotImplementedException();
+            
+        }
+
+        private void FixedUpdate()
+        {
+            if (Input.GetKey(KeyCode.A))
+                UnitModel.Velocity = (250f * Time.deltaTime) * Vector2.left;
+            else if (Input.GetKey(KeyCode.D))
+                UnitModel.Velocity = (250f * Time.deltaTime) * Vector2.right;
+            else if (Input.GetKey(KeyCode.W))
+                UnitModel.Velocity = (250f * Time.deltaTime) * Vector2.up;
+            else if (Input.GetKey(KeyCode.S))
+                UnitModel.Velocity = (250f * Time.deltaTime) * Vector2.down;
+            else
+                UnitModel.Velocity = Vector2.zero;
+            rigidbody2D.velocity = UnitModel.Velocity;
         }
     }
 }
