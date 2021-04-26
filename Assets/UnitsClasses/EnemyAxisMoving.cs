589fe9bc65c21481e7a503ce46fa5fc1dd44e9da
Vurado-t <vladik.mihalev@yandex.ru>
2021-04-26 21:56:36 +0500

    Many Ememies

---
+++ Assets/UnitsClasses/EnemyAxisMoving.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace UnitsClasses
+{
+    public class EnemyMoving : UnitMoving
+    {
+        private const float LeftBorder = -10f;
+        private const float RightBorder = 10f;
+
+        private void Start()
+        {
+            MovingDirection = Vector2.right;
+        }
+
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            Rigidbody.drag = 5f;
+        }
+
+        private new void FixedUpdate()
+        {
+            base.FixedUpdate();
+            Move();
+        }
+
+        private void Move()
+        {
+            var position = Transform.position;
+            if (position.x < LeftBorder || position.x > RightBorder)
+            {
+                MovingDirection = -MovingDirection;
+            }
+            Move(MovingDirection);
+        }
+    }
+}
---
+++ Assets/UnitsClasses/EnemyCloseAttack.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Timers;
+using UnityEngine;
+using Timer = System.Timers.Timer;
+
+namespace UnitsClasses
+{
+    public class EnemyAttack : UnitComponent
+    {
+        private readonly Timer timer = new Timer();
+        private ElapsedEventHandler SetAttackFlagByTimer;
+        private HealthSystem target;
+        private bool isAttacking;
+
+        private new void Awake()
+        {
+            base.Awake();
+            timer.Interval = 500;
+            timer.AutoReset = true;
+            SetAttackFlagByTimer = (obj, args) => isAttacking = true;
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (target != null)
+            {
+                return;
+            }
+
+            target = other.gameObject.GetComponent<HealthSystem>();
+            timer.Elapsed += SetAttackFlagByTimer;
+            timer.Start();
+        }
+
+        private void OnCollisionStay2D(Collision2D other)
+        {
+            if (other.gameObject.GetComponent<HealthSystem>() == target && isAttacking)
+            {
+                Attack(target);
+                isAttacking = false;
+            }
+        }
+
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            if (other.gameObject.GetComponent<HealthSystem>() == target)
+            {
+                timer.Stop();
+                timer.Elapsed -= SetAttackFlagByTimer;
+                SetAttackFlagByTimer = null;
+                target = null;
+            }
+        }
+
+        private void Attack(HealthSystem otherHealthSystem)
+        {
+            otherHealthSystem.Health -= 10;
+        }
+    }
+}
