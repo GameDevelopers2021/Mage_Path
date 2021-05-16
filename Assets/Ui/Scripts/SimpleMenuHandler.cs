721ffa59fdf8a9d8641d4a5a5b91dd946da0848f
Vurado-t <mixplay20102002@gmail.com>
2021-05-16 17:42:20 +0500

    fixed bug with timeScale when scenes are switching

--- Assets/Ui/Scripts/SimpleMenuHandler.cs
+++ Assets/Ui/Scripts/SimpleMenuHandler.cs
@@ -6,11 +6,23 @@ namespace Ui.Scripts
 {
     public class SimpleMenuHandler : MonoBehaviour
     {
-        public void GoToNextScene(string sceneName) => SceneManager.LoadScene(sceneName);
-        
-        public void GoToNextScene(int sceneIndex) => SceneManager.LoadScene(sceneIndex);
+        public void GoToNextScene(string sceneName)
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene(sceneName);
+        }
 
-        public void GoToNextScene() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        public void GoToNextScene(int sceneIndex)
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene(sceneIndex);
+        }
+
+        public void GoToNextScene()
+        {
+            Time.timeScale = 1;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
 
         public void ExitGame()
         {
