e4660ed59d29025fec3fd724f65c77e536b917f2
Vurado-t <mixplay20102002@gmail.com>
2021-05-24 08:22:35 +0500

    release

--- Assets/Ui/Scripts/HelpTriggerBehaviour.cs
+++ Assets/Ui/Scripts/HelpTriggerBehaviour.cs
@@ -9,20 +9,16 @@ namespace Ui.Scripts
         [SerializeField] private GameObject menuToOpen;
         [SerializeField] private bool isOpeningOnce;
 
-        private bool wasOpened;
-
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (!other.CompareTag("Player"))
             {
                 return;
             }
-
-            if (isOpeningOnce && wasOpened)
-                return;
-
-            wasOpened = true;
+            
             openingHandler.OpenMenu(menuToOpen);
+            if (isOpeningOnce)
+                gameObject.SetActive(false);
         }
     }
 }
