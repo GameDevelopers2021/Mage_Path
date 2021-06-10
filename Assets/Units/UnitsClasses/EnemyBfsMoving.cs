854501435bdc54e88f31605a965aced4f7981528
Vurado-t <mixplay20102002@gmail.com>
2021-06-10 18:45:20 +0500

    zombie animation fixed

--- Assets/Units/UnitsClasses/EnemyBfsMoving.cs
+++ Assets/Units/UnitsClasses/EnemyBfsMoving.cs
@@ -14,23 +14,9 @@ namespace Units.UnitsClasses
             Move();
 
             var objectTransform = transform;
-            var scale = objectTransform.localScale;
-            var signX = Math.Sign(Rigidbody.velocity.x);
-            var signY = Math.Abs(Rigidbody.velocity.y) < 1e-4 ? 0 : Math.Sign(Rigidbody.velocity.y);
-            var rotation = transform.rotation.eulerAngles;
-
-            scale.x = signX == 0 ? scale.x : Math.Abs(scale.x) * signX;
-            rotation.z = signY != 0 ? 90 * signY : 0;
-
-            scale.x = rotation.z != 0 
-                ? Math.Abs(scale.x) 
-                : signX == 0 ? scale.x : Math.Abs(scale.x) * signX;
-
-            var rotationQuaternion = new Quaternion(0, 0, 0, 0);
-            rotationQuaternion.eulerAngles = rotation;
+            var angle = Vector2.SignedAngle(objectTransform.right, Rigidbody.velocity);
             
-            objectTransform.rotation = rotationQuaternion;
-            objectTransform.localScale = scale;
+            objectTransform.Rotate(Vector3.forward, angle);
         }
 
         private void Move()
